c3ae4f82e22d19e64bc43c9567067fe9e20b0f9e
mohdzahid034 <mohdansari0935@gmail.com>
2026-02-09 23:01:30 +0530

    Add MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("DB error:", err));
+  
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
